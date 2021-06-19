449c0991eba300ed72ed44824c8a1e7e3482430e
GraciousGPal <14353802+GraciousGpal@users.noreply.github.com>
2021-06-19 16:36:00 +0100

    - Increased Default buffer size. - Reverted the change from last patch.

--- main.py
+++ main.py
@@ -78,7 +78,7 @@ class Server:
             while True:
                 message = await listen_for_messages(user)
                 if message is None:
-                    continue
+                    break
                 messages = message.split('\00')
                 try:
                     messages.remove('')
